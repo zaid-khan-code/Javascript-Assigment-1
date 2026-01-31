590462775accfd0f28834308bfae2b0bd5db8a43
Zaid Khan <zaidbinasif468@gmail.com>
2026-01-31 17:47:21 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,78 @@
+// Task Fist Started
+/*  {
+    var firstVar = "var";
+    console.log(firstVar);
+    }
+    {
+        let firstVar ="let" ;
+        console.log(firstVar);
+        }
+        console.log(firstVar);
+        */
+
+// Task Fist Completed
+
+// Task Second Started
+
+/*
+var firstVar = "hello var";
+console.log(firstVar);
+firstVar += " world";
+console.log(firstVar);
+let firstLet = "hello let";
+console.log(firstLet);
+firstLet += " world";
+console.log(firstLet);
+const fistCnst = "hello const";
+try {
+  fistCnst = "hello const";
+} catch (error) {
+  console.error(error);
+}
+*/
+
+
+// Task Second Completed
+
+
+
+
+// Task Third Started
+
+/*
+
+for (var i = 0; i < 10; i++) {
+    setTimeout(() => {
+        console.log(` var ${i} and ${1000} ms`);
+
+    }, 1000)
+}
+
+
+for (let i = 0; i < 10; i++) {
+    setTimeout(() => {
+        console.log(` let ${i} and ${2000} ms`);
+
+    }, 2000)
+}
+
+try {
+
+    for (const i = 0; i < 10; i++) {
+        setTimeout(() => {
+            console.log(`const ${i} and ${3000} ms`);
+
+        }, 3000)
+    }
+} catch (error) {
+    console.error(error);
+
+}
+
+
+
+
+*/
+
+
+// Task Third Completed
---
+++ script2.js
@@ -0,0 +1,106 @@
+// Task Second (i) Started
+/*  
+ function messageGenerator(user,  course,per) {
+    let  message = `
+    Hello ${user},
+    You have received the Scholarship from the Umm Al-Qura University in Saudia Arabia.
+    This is because of your Grades ${per} From ${course}.
+
+
+    Thanks you will receive you other details via email`
+ console.log(message);
+    
+ }
+
+
+messageGenerator("Zaid Bin Asif Khan","BSEK 2 year",99.96) 
+*/
+
+// Task Second (i) Completed
+
+
+// Task Second (ii) Started
+
+
+/*
+
+function resultGen(percentage) {
+    const grades = percentage > 50 ? `You have passed the Tougest exams of the centuries` : `You have failed the Easyest task of the history`;
+    console.log(grades);
+}
+
+resultGen(90)
+
+*/
+
+
+// Task Second (ii)  Completed
+
+
+
+// Task Second (iii)   Started
+
+/*
+const obj = {
+    name: "Muhammad Zaid Bin Asif Bin Ashraf Khan Khalil", // i don't know more about my grand grand praents
+    normalFun: function normalFunCtion() {
+        console.log(`This is a normal function this ${this.name}`);
+        
+    },
+    arrowFun: () => {
+        console.log(`This is a Arrow function this ${this.name}`);
+            
+    },
+}
+
+obj.arrowFun()
+obj.normalFun()
+*/
+
+
+// Task Second (iii)  Completed
+
+
+// Task Second (vi) Started
+/* 
+const nums = [2, 4, 6, 8];
+
+const implicitReturn = nums.map(num => num * 2);
+const explicitReturn = nums.map(num => { return num * 2 });
+console.log(implicitReturn);
+console.log(explicitReturn);
+
+
+
+
+
+*/
+// Task Second (vi) Completed
+
+
+
+// Task Second (v) Started
+
+// /*   
+const arrowFun = (callback) => {
+    console.log(callback());
+
+}
+function normalFun(callback) {
+    console.log(callback());
+
+}
+arrowFun(writenWorld)
+normalFun(writenWorld)
+
+
+function writenWorld() {
+    return "hello world"
+}
+
+// */
+
+
+
+
+// Task Second (v) Completed
